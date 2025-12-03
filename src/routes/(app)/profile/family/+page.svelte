1cc28ad324a53a8a893c75adf0c27bd1316d82cd
zachczx <zixian.chen@gmail.com>
2025-12-03 08:15:00 +0800

    feat: redesigned family profile UI

--- src/routes/(app)/profile/family/+page.svelte
+++ src/routes/(app)/profile/family/+page.svelte
@@ -20,7 +20,7 @@
 	import SegmentedControl from '$lib/ui/SegmentedControl.svelte';
 	import type { ChangeEventHandler } from 'svelte/elements';
 	import MaterialSymbolsDelete from '$lib/assets/svg/MaterialSymbolsDelete.svelte';
-	import { invalidateAll } from '$app/navigation';
+	import { goto, invalidateAll } from '$app/navigation';
 	import MaterialSymbolsPerson from '$lib/assets/svg/MaterialSymbolsPerson.svelte';
 	import MaterialSymbolsCheck from '$lib/assets/svg/MaterialSymbolsCheck.svelte';
 	import MaterialSymbolsFamilyGroup from '$lib/assets/svg/MaterialSymbolsFamilyGroup.svelte';
@@ -35,7 +35,7 @@
 	dayjs.extend(timezone);
 
 	const user = createQuery(createUserQueryOptions);
-	const family = createQuery(familyQueryOptions);
+	const families = createQuery(familyQueryOptions);
 	const currentInvite = createQuery(inviteQueryOptions);
 	const tanstackClient = useQueryClient();
 
@@ -68,12 +68,12 @@
 		}
 	}
 
-	async function deleteHandler(userId: string) {
-		if (!user.isSuccess || !family.data) return;
+	async function deleteHandler(userId: string, family: FamilyDB) {
+		if (!user.isSuccess || !family) return;
 		spinner = true;
 
 		try {
-			const result = await pb.collection('families').update(family.data.id, {
+			const result = await pb.collection('families').update(family.id, {
 				'members-': userId
 			});
 
@@ -87,20 +87,20 @@
 		}
 	}
 
-	async function handleInvite() {
-		if (!family.data) return;
+	async function handleInvite(family: FamilyDB) {
+		if (!family) return;
 
 		invited = true;
 
 		const inviteResponse = await pb.collection('invites').create({
-			family: family.data.id,
-			familyNameSnapshot: family.data?.name,
-			ownerEmailSnapshot: family.data.expand?.owner?.email,
+			family: family.id,
+			familyNameSnapshot: family.name,
+			ownerEmailSnapshot: family.expand?.owner?.email,
 			userEmail: invitee,
 			status: 'pending'
 		});
 
-		await pb.collection('families').update(family.data.id, {
+		await pb.collection('families').update(family.id, {
 			'activeInvites+': inviteResponse.id
 		});
 
@@ -113,166 +113,210 @@
 			invited = false;
 		}, 3000);
 	}
+
+	async function leaveFamily(userId: string | undefined, family: FamilyDB) {
+		if (!family) return;
+
+		try {
+			const result = await pb.collection('families').update(family.id, {
+				'members-': userId
+			});
+
+			if (result) {
+				addToast('success', 'Successfully left family!');
+				await tanstackClient.refetchQueries(familyRefetchOptions());
+				goto('/profile/family');
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	let section = $state('');
+
+	$effect(() => {
+		if (families.isSuccess && families.data.length > 0) {
+			section = families.data[0].id;
+		}
+	});
 </script>
 
 <PageWrapper title="Family" {pb} largeScreenCenter={true}>
 	<div
-		class="lg:bg-base-200 grid w-full rounded-2xl max-lg:h-full max-lg:grid-rows-[1fr_auto] lg:h-min lg:max-w-lg lg:justify-self-center lg:p-8 lg:shadow-md"
+		class="lg:bg-base-200 grid w-full rounded-2xl lg:h-min lg:max-w-lg lg:justify-self-center lg:p-8 lg:shadow-md"
 	>
 		<h1 class="text-primary mb-4 text-center text-4xl font-bold max-lg:hidden">Family</h1>
 
 		<div class="grid w-full content-start gap-8">
-			{#if currentInvite.isSuccess && currentInvite.data && currentInvite.data.status !== 'completed'}
-				<section
-					class="border-base-300 bg-info text-info-content grid min-h-18 gap-4 rounded-2xl border p-4 text-lg font-semibold"
-				>
-					<a
-						href="/profile/family/invite?i={currentInvite.data.family}"
-						class="flex items-center gap-4"
-						><MdiAlertCircle class="size-[2em]" />You received an invite <MaterialSymbolsArrowRightAlt
-							class="-ms-3 size-[1.3em]"
-						/></a
-					>
-				</section>
-			{/if}
-
-			<section class="border-base-300 grid min-h-18 gap-4 rounded-2xl border bg-white/70 p-4">
-				<h2 class="flex items-center gap-4 text-xl font-bold">
-					<MaterialSymbolsFamilyGroup class="size-[1.3em]" />Your Family
-				</h2>
-				{#if family.isPending && !family.data}
-					<div class="custom-loader"></div>
-				{/if}
-				{#if family.isSuccess && family.data}
-					<div>
-						{#if family.data.owner === pb.authStore.record?.id}
-							You're the owner of "{family.data.name}".
-						{:else}
-							You are in
-							<span class="text-primary font-bold">
-								{#if family.data.expand?.owner?.name}
-									{family.data.expand?.owner?.name}
-								{:else}
-									{@const nameFromEmail = cleanEmail(family.data.expand?.owner?.email)}
-									{nameFromEmail}
-								{/if}</span
-							>'s family.
-						{/if}
-					</div>
-				{/if}
-				<div class="grid grid-cols-1 justify-items-start">
-					<button class="flex items-center gap-2"
-						>Switch to another family<MaterialSymbolsArrowRightAlt /></button
-					>
-				</div>
-			</section>
-			<section class="border-base-300 grid min-h-18 gap-4 rounded-2xl border bg-white/70 p-4">
-				<h2 class="text-xl font-bold">Members ({family.data?.members.length})</h2>
-				{#if family.isPending && !family.data}
-					<div class="custom-loader"></div>
-				{/if}
-
-				<ul class="grid list-disc">
-					{#if family.isSuccess && family.data}
-						{#each family.data.expand?.members as member, i}
-							<li class="flex items-center">
-								<div class="flex grow items-center gap-4 py-1">
-									<MaterialSymbolsPerson />
-									{#if member.name}
-										{member.name}
-									{:else}
-										{member.email}
-									{/if}
-									<div class="flex items-center gap-2">
-										{#if family.data.owner === member.id}
-											<span class="btn btn-outline btn-xs border-base-content/50 border">Owner</span
-											>
-										{/if}
-										{#if pb.authStore.record?.id === member.id}
-											<span class="btn btn-outline btn-xs border-base-content/50 border">You</span>
-										{/if}
-									</div>
-								</div>
-
-								<div class="dropdown dropdown-end">
-									<div tabindex="0" role="button" class="btn btn-ghost drawer-button px-2 py-0">
-										<MaterialSymbolsMoreVert class="" />
-									</div>
-									<ul
-										class="dropdown-content menu rounded-box bg-base-100 text-md text-base-content z-1 min-w-32 shadow-lg"
-									>
-										<button
-											onclick={() => modals[i].showModal()}
-											class="btn btn-ghost flex w-full items-center gap-2 rounded-xl"
-											><MaterialSymbolsPersonRemove class="size-[1.3em]" />Remove</button
-										>
-									</ul>
-								</div>
-							</li>
-						{/each}
-					{/if}
-				</ul>
-			</section>
-
-			<section class="border-base-300 grid min-h-18 rounded-2xl border bg-white/70 p-4">
-				<h2 class="mb-4 text-xl font-bold">Invite Someone</h2>
-				{#if family.isPending && !family.data}
-					<div class="custom-loader"></div>
-				{/if}
-				{#if family.isSuccess && family.data}
-					<form onsubmit={handleInvite} class="join mb-2">
-						<input
-							type="email"
-							placeholder="Enter email address"
-							class="input join-item focus-within:border-neutral w-full font-medium focus-within:outline-0"
-							bind:value={invitee}
-							required
-						/>
+			<nav class="flex items-center gap-2">
+				{#if families.isSuccess}
+					{#each families.data as family}
 						<button
-							class={[
-								'join-item btn btn-neutral btn-0 flex min-w-20 items-center gap-2 border-dashed',
-								invited && 'btn-success'
-							]}
+							onclick={() => {
+								section = family.id;
+							}}
+							class="w-full max-w-48 border-b-2 pb-1 text-center {section === family.id
+								? 'border-b-neutral text-neutral font-bold'
+								: 'text-base-content/80 border-b-transparent'}"
 						>
-							{#if !invited}
-								Invite
-								<!-- <MaterialSymbolsShare class="size-6" /> -->
+							{#if family.owner === pb.authStore.record?.id}
+								My Cubby
+							{:else if family.expand?.owner?.email}
+								{cleanEmail(family.expand?.owner?.email)}
 							{:else}
-								<MaterialSymbolsCheck class="size-6" />
+								{family.owner}
 							{/if}
 						</button>
-					</form>
-					<p class="text-xs">They'll need to accept your invitation.</p>
+					{/each}
 				{/if}
-			</section>
+			</nav>
+
+			{#each families.data as family}
+				{#if family.id === section}
+					{#if currentInvite.isSuccess && currentInvite.data && currentInvite.data.status !== 'completed'}
+						<section
+							class="border-base-300 bg-info text-info-content grid min-h-18 gap-4 rounded-2xl border p-4 text-lg font-semibold"
+						>
+							<a
+								href="/profile/family/invite?i={currentInvite.data.family}"
+								class="flex items-center gap-4"
+								><MdiAlertCircle class="size-[2em]" />You received an invite <MaterialSymbolsArrowRightAlt
+									class="-ms-3 size-[1.3em]"
+								/></a
+							>
+						</section>
+					{/if}
+
+					<section class="border-base-300 grid min-h-18 gap-4 rounded-2xl border bg-white/70 p-4">
+						<h2 class="text-xl font-bold">Members ({family.members.length})</h2>
+						{#if families.isPending && !families.data}
+							<div class="custom-loader"></div>
+						{/if}
+
+						<ul class="grid list-disc">
+							{#if families.isSuccess && families.data}
+								{#each family.expand?.members as member, i}
+									<li class="flex items-center">
+										<div class="flex grow items-center gap-2 py-1">
+											<MaterialSymbolsPerson class="me-2" />
+											{#if member.name}
+												{member.name}
+											{:else}
+												{member.email}
+											{/if}
+											{#if pb.authStore.record?.id === member.id}
+												<span class="text-sm opacity-60">(You)</span>
+											{/if}
+											<div class="flex items-center gap-2">
+												{#if family.owner === member.id}
+													<span class="btn btn-outline border-base-content/50 h-auto border"
+														>Owner</span
+													>
+												{/if}
+											</div>
+										</div>
+
+										{#if family.owner === pb.authStore.record?.id && member.id !== pb.authStore.record?.id}
+											<div class="dropdown dropdown-end">
+												<div
+													tabindex="0"
+													role="button"
+													class="btn btn-ghost drawer-button px-2 py-0"
+												>
+													<MaterialSymbolsMoreVert class="" />
+												</div>
+												<ul
+													class="dropdown-content menu rounded-box bg-base-100 text-md text-base-content z-1 min-w-32 shadow-lg"
+												>
+													<button
+														onclick={() => modals[i].showModal()}
+														class="btn btn-ghost flex w-full items-center gap-2 rounded-xl"
+														><MaterialSymbolsPersonRemove class="size-[1.3em]" />Remove</button
+													>
+												</ul>
+											</div>
+										{/if}
+									</li>
+								{/each}
+							{/if}
+						</ul>
+
+						{#if family.owner !== pb.authStore.record?.id}
+							<div class="border-t-base-300/70 flex justify-center border-t-2 pt-3">
+								<button
+									class="btn btn-ghost text-error flex items-center gap-2 py-0"
+									onclick={() => leaveFamily(pb.authStore.record?.id, family)}
+									>Leave Cubby<MaterialSymbolsArrowRightAlt /></button
+								>
+							</div>
+						{/if}
+					</section>
+
+					{#if family.owner === pb.authStore.record?.id}
+						<section class="border-base-300 grid min-h-18 rounded-2xl border bg-white/70 p-4">
+							<h2 class="mb-4 text-xl font-bold">Invite Someone</h2>
+							{#if families.isPending && !families.data}
+								<div class="custom-loader"></div>
+							{/if}
+							{#if families.isSuccess && families.data}
+								<form onsubmit={() => handleInvite(family)} class="join mb-2">
+									<input
+										type="email"
+										placeholder="Enter email address"
+										class="input join-item focus-within:border-neutral w-full font-medium focus-within:outline-0"
+										bind:value={invitee}
+										required
+									/>
+									<button
+										class={[
+											'join-item btn btn-neutral btn-0 flex min-w-20 items-center gap-2 border-dashed shadow-none',
+											invited && 'btn-success'
+										]}
+									>
+										{#if !invited}
+											Invite
+											<!-- <MaterialSymbolsShare class="size-6" /> -->
+										{:else}
+											<MaterialSymbolsCheck class="size-6" />
+										{/if}
+									</button>
+								</form>
+								<p class="text-xs">They'll need to accept your invitation.</p>
+							{/if}
+						</section>
+					{/if}
+				{/if}
+			{/each}
 		</div>
 	</div>
 </PageWrapper>
 
-{#if family.isSuccess && family.data}
-	{#each family.data.expand?.members as member, i}
-		<dialog bind:this={modals[i]} class="modal modal-bottom sm:modal-middle">
-			<div class="modal-box grid gap-8">
-				<div
-					class="bg-error/10 text-error flex aspect-square size-20 items-center justify-center justify-self-center overflow-hidden rounded-full"
-				>
-					<MaterialSymbolsPersonRemove class="ms-2.5 size-12" />
-				</div>
-				<div class="grid gap-4">
-					<h2 class="text-2xl font-bold">Remove Member?</h2>
-					<ul class="ms-6 list-disc space-y-2">
-						<li>This will revoke their access to your family's logs.</li>
-					</ul>
-				</div>
-				<div class="grid grid-cols-1 gap-4">
-					<button class="btn btn-error btn-lg" onclick={() => deleteHandler(member.id)}
-						>Remove</button
+{#if families.isSuccess && families.data}
+	{#each families.data as family}
+		{#each family.expand?.members as member, i}
+			<dialog bind:this={modals[i]} class="modal modal-bottom sm:modal-middle">
+				<div class="modal-box grid gap-8">
+					<div
+						class="bg-error/10 text-error flex aspect-square size-20 items-center justify-center justify-self-center overflow-hidden rounded-full"
 					>
-					<form method="dialog" class="">
-						<button class="btn btn-outline btn-lg w-full">Cancel</button>
-					</form>
+						<MaterialSymbolsPersonRemove class="ms-2.5 size-12" />
+					</div>
+					<div class="grid gap-4">
+						<h2 class="text-2xl font-bold">Remove Member?</h2>
+						<ul class="ms-6 list-disc space-y-2">
+							<li>This will revoke their access to your family's logs.</li>
+						</ul>
+					</div>
+					<div class="grid grid-cols-1 gap-4">
+						<button class="btn btn-error btn-lg" onclick={() => deleteHandler(member.id, family)}
+							>Remove</button
+						>
+						<form method="dialog" class="">
+							<button class="btn btn-outline btn-lg w-full">Cancel</button>
+						</form>
+					</div>
 				</div>
-			</div>
-		</dialog>
+			</dialog>
+		{/each}
 	{/each}
 {/if}
