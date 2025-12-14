b833572a9034208f9a0dfb0ee092045aed3a58a0
Zixian <109778851+zachczx@users.noreply.github.com>
2025-12-14 23:36:00 +0800

    fix: add logData to ActionCard instances

--- src/routes/app/[category]/+page.svelte
+++ src/routes/app/[category]/+page.svelte
@@ -10,10 +10,11 @@
 	import FluentEmojiFlatStopwatch from '$lib/assets/expressive-icons/FluentEmojiFlatStopwatch.svelte';
 	import ActionCard from '$lib/ui/ActionCard.svelte';
 	import { createQuery } from '@tanstack/svelte-query';
-	import { allTrackersQueryOptions } from '$lib/queries';
+	import { allLogsQueryOptions, allTrackersQueryOptions } from '$lib/queries';
 	import { getColoredTrackers, getFamilyColor, getTrackerIcon } from '$lib/mapper.js';
 	import SkeletonActionCard from '$lib/ui/SkeletonActionCard.svelte';
 	import EmptyCorgi from '$lib/assets/empty.webp?w=200&enhanced';
+	import { getTrackerStatus } from '$lib/notification.js';
 
 	let { data } = $props();
 
@@ -33,6 +34,7 @@
 	});
 
 	const trackersDb = createQuery(allTrackersQueryOptions);
+	const allLogsDb = createQuery(allLogsQueryOptions);
 
 	let currentTrackers = $derived.by(() => {
 		if (!trackersDb.isSuccess || !trackersDb.data) return;
@@ -43,6 +45,37 @@
 
 		return getColoredTrackers(categoryTrackers);
 	});
+
+	let logsByTracker = $derived.by(() => {
+		const map = new Map();
+		if (!allLogsDb.isSuccess || !allLogsDb.data) return map;
+
+		for (const log of allLogsDb.data) {
+			if (!map.has(log.tracker)) {
+				map.set(log.tracker, []);
+			}
+
+			map.get(log.tracker).push(log);
+		}
+
+		return map;
+	});
+
+	let logs = $derived.by(() => {
+		if (!trackersDb.isSuccess || !trackersDb.data || !allLogsDb.isSuccess || !allLogsDb.data)
+			return;
+
+		return currentTrackers?.map((tracker) => {
+			const associatedLogs = logsByTracker.get(tracker.id) || [];
+
+			return {
+				trackerName: tracker.name,
+				trackerData: tracker,
+				logData: associatedLogs,
+				notification: getTrackerStatus(associatedLogs)
+			};
+		});
+	});
 </script>
 
 <PageWrapper title={data.category.charAt(0).toUpperCase() + data.category.slice(1)} {pb}>
@@ -52,17 +85,18 @@
 				{#if trackersDb.isPending}
 					<SkeletonActionCard />
 					<SkeletonActionCard />
-				{:else if currentTrackers && currentTrackers.length > 0}
-					{#each currentTrackers as tracker}
+				{:else if logs && logs.length > 0}
+					{#each logs as log}
 						<ActionCard
 							options={{
-								tracker: tracker,
-								title: tracker.display,
-								route: `/${tracker.category}/${tracker.id}`,
-								icon: getTrackerIcon(tracker.name),
+								tracker: log.trackerData,
+								title: log.trackerData.display,
+								route: `/${log.trackerData.category}/${log.trackerData.id}`,
+								logs: log.logData,
+								icon: getTrackerIcon(log.trackerData.name),
 								button: {
-									text: tracker.actionLabel,
-									status: buttonStatuses?.[tracker.name]
+									text: log.trackerData.actionLabel,
+									status: buttonStatuses?.[log.trackerData.name]
 								}
 							}}
 						></ActionCard>
