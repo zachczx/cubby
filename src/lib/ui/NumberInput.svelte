167aae5c869a4c15f0ef098f9112bdcf94633eb6
Zixian <109778851+zachczx@users.noreply.github.com>
2025-12-15 19:15:00 +0800

    feat: add new tracker page

---
+++ src/lib/ui/NumberInput.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { NumberInput } from '@ark-ui/svelte/number-input';
+
+	let { value = $bindable('1') } = $props();
+</script>
+
+<NumberInput.Root name="interval" bind:value min={1}>
+	<!-- <NumberInput.Label>Count</NumberInput.Label> -->
+	<NumberInput.Control class="input input-lg w-full p-0">
+		<NumberInput.DecrementTrigger
+			class="bg-neutral/40 text-primary-content h-full min-w-16 rounded-l-md px-8 text-3xl"
+			>-</NumberInput.DecrementTrigger
+		>
+		<NumberInput.Input class="text-center" />
+		<NumberInput.IncrementTrigger
+			class="text-primary-content bg-neutral/40 h-full min-w-16 rounded-r-md px-2 text-3xl"
+			>+</NumberInput.IncrementTrigger
+		>
+	</NumberInput.Control>
+</NumberInput.Root>
